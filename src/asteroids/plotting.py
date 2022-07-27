9fc02c06038f104f6ad1f49f263ff77154bcdcea
Sagi Shadur <saroad2@gmail.com>
2022-07-27 22:42:59 +0300

    Revert "Refactor plotting"
    This reverts commit 426722b706ca77cd7b6d78a9c0c77e20d7dbf1cc.

--- src/asteroids/plotting.py
+++ src/asteroids/plotting.py
@@ -3,23 +3,24 @@ from typing import List
 
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.signal import savgol_filter
 
 from asteroids.history import HistoryPoint
 
 
-def plot_all(history: List[HistoryPoint], output_dir: Path):
+def plot_all(history: List[HistoryPoint], window: int, output_dir: Path):
     output_dir.mkdir(exist_ok=True)
     for field in HistoryPoint.fields():
         values = [getattr(history_point, field) for history_point in history]
-        plot_smoothly(values=values, name=field, output_dir=output_dir)
+        plot_moving_average(
+            values=values, window=window, name=field, output_dir=output_dir
+        )
 
 
-def plot_smoothly(values, name, output_dir):
-    smooth_values = savgol_filter(values, window_length=len(values) // 10, polyorder=3)
-    plt.plot(np.arange(len(smooth_values)), smooth_values)
+def plot_moving_average(values, window, name, output_dir):
+    mean_values = np.convolve(values, np.ones(window), mode="valid") / window
+    plt.plot(np.arange(window, len(values) + 1), mean_values)
     plt.xlabel("episode")
     plt.ylabel(name)
-    plt.title(f"{name} smoothed")
+    plt.title(f"{name} rolling average ({window=})")
     plt.savefig(output_dir / f"{name}.jpg")
     plt.clf()
